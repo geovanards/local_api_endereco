69779b59f535fe508b6160931c7a5d6c16bea6cb
getitup3548 <159202999+getitup3548@users.noreply.github.com>
2024-05-08 20:15:56 -0300

    api_2

--- scripts.js
+++ scripts.js
@@ -2,8 +2,24 @@ function enviarDados() {
     const formulario = document.getElementById("meuFormulario");
     const formData = new FormData(formulario);
     const jsonData = {};
+
+
     formData.forEach((value, key) => {
         jsonData[key] = value;
     })};
 
-    
+    fetch('http://localhost:3000/user',{
+    })
+    .then(response=>{
+        if (response.ok) {
+            throw new Error('Erro ao enviar dados');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Resposta do servidor:', data);
+        alert(data.message)
+    })
+    .catch(error =>{
+        console.log('Erro:')
+    })
